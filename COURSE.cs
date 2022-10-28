c407fed15a72b5cb7f18b3ba9ba96a07de629051
ashwin333 <ashwinpatil832@gmail.com>
2022-10-28 13:21:51 +0530

    Add project files.

---
+++ COURSE.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class COURSE
+    {
+        private int id;
+        private string name;
+        double fees;
+
+        public void add(int id, string name, double fees)
+        {
+            this.id = id;
+            this.name = name;
+            this.fees = fees;
+        }
+        public string Print()
+        {
+            return $" Course:{id}/{name}/{fees}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            COURSE c1 = new COURSE();
+            c1.add(83, "Ashwin", 75.8);
+            Console.WriteLine(c1.Print());
+        }
+    }
+    
+}
